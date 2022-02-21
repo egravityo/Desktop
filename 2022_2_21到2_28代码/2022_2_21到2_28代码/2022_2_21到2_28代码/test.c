7662d89fd4c5b4e3f75c5bf1899eff1270ec9aeb
陈镖任 <511766439@qq.com>
2022-02-21 17:19:42 +0800

    同意

--- 2022_2_21到2_28代码/2022_2_21到2_28代码/2022_2_21到2_28代码/test.c
+++ 2022_2_21到2_28代码/2022_2_21到2_28代码/2022_2_21到2_28代码/test.c
@@ -3,9 +3,44 @@
 #include<string.h>
 #include<stdlib.h>
 #include<errno.h>
-
-int mian()
-{
-
-	return 0;
-}
+#include<assert.h>
+//
+//int i;//全局变量，默认是0
+//int main()
+//{
+//	i--;//-1
+//    //sizeof这个操作符，算出的结果的类型是unsigned int
+//    
+//	if (i > sizeof(i)) {
+//		printf(">\n");
+//	}
+//	else {
+//		printf("<\n");
+//	}
+//	return 0;
+//}
+//----------------------------------
+//写一个函数,可以逆序一个字符串
+//void fun(char* p,int sz)
+//{
+//	assert(p);
+//	char* left = p;
+//	char* right = p+ sz - 1;
+//	while (left <=  right)
+//	{
+//		char temp = *left;
+//		*left = *right;
+//		*right = temp;
+//		left++;
+//		right--;
+//	}
+//}
+//int main()
+//{
+//	char str[] = "abcdefg";
+//	int sz = strlen(str);
+//	fun(str,sz);
+//	puts(str);
+//	return 0;
+//}
+//-----------------------
